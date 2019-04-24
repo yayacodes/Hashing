1dde094595563697de930dc0b3522ac2bb2b5094
Yaya Awad <yehiakawad@gmail.com>
2019-04-24 18:53:02 -0400

    Add files via upload

---
+++ Hashing.java
@@ -0,0 +1,203 @@
+// On my honor:
+//
+// - I have not used code obtained from another student,
+// or any other unauthorized source, either modified or unmodified.
+//
+// - All code and documentation used in my program is either my original
+// work, or was derived, by me, from the source code provided by the assignment.
+//
+// - I have not discussed coding details about this project with anyone
+// other than my instructor, teaching assistants assigned to this
+// course, except via the message board for the course. I understand that I
+// may discuss the concepts of this program with other students, and that
+// another student may help me debug my program so long as neither of us
+// writes anything during the discussion or modifies any computer file
+// during the discussion. I have violated neither the spirit nor letter of this restriction.
+//
+
+
+import java.util.*;
+import java.io.*;
+
+public class Hashing implements Set<String> {
+
+    private final static int table_size = 10000000;
+
+    String[] arrSet;
+
+    Hashing(){
+        arrSet = new String[table_size];
+        for(int i = 0; i < arrSet.length; i++)
+            arrSet[i] = null;
+    }
+
+    Hashing(int size){
+        arrSet = new String[size];
+        for(int i = 0; i <arrSet.length; i++)
+            arrSet[i] = null;
+    }
+
+    private int hashingCode(String s) {
+        int code = 0;
+        for (int i = 0; i < s.length(); i++) {
+            code += s.charAt(i) * Math.pow(139, s.length() - i - 1);
+        }
+        if(code < 0)
+            code*=-1;
+
+        return code;
+    }
+
+    @Override
+    public int size() {
+        int count = 0;
+        for(int i = 0; i < arrSet.length; i++){
+            if(arrSet[i] != null)
+                count++;
+        }
+
+        return count;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        for(int i = 0; i < arrSet.length; i++){
+            if(arrSet[i] != null)
+                return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        String s = o.toString();
+        String lowString = s.toLowerCase();
+        boolean loop = true;
+
+        int index = hashingCode(lowString) % arrSet.length;
+
+        while(index <= arrSet.length - 2){
+            if (arrSet[index] != null && arrSet[index].equals(lowString)) {
+                loop = false;
+                return true;
+            }
+            else {
+                index = (index + 1) % arrSet.length;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Iterator<String> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(String s) {
+        String lowString = s.toLowerCase();
+        int index = hashingCode(lowString) % arrSet.length;
+
+        while(true){
+            if(arrSet[index] != null && arrSet[index].equals(lowString)){
+                return false;
+            }
+
+            else if(arrSet[index] != null){
+                index = (index + 1) % arrSet.length;
+            }
+
+            else{
+                arrSet[index] = lowString;
+                return true;
+            }
+        }
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        String s = o.toString();
+        String lowString = s.toLowerCase();
+        boolean loop = true;
+
+        int index = hashingCode(lowString) % arrSet.length;
+
+        if(contains(lowString) == true) {
+            while (index <= arrSet.length - 2) {
+                if (arrSet[index] != null && arrSet[index].equals(lowString)) {
+                    arrSet[index] = null;
+                    return true;
+                }
+                else{
+                    index = (index + 1) % arrSet.length;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends String> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    public static void main(String[] args) throws IOException{
+        Hashing arr = new Hashing();
+
+        try {
+            Scanner scanner = new Scanner(new FileReader("words"));
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+                arr.add(scanner.nextLine());
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Scanner input = new Scanner(System.in);
+
+        String word = input.nextLine();
+
+        if(arr.contains(word) == true)
+            System.out.println("ok");
+
+        if(arr.contains(word) == false)
+            System.out.println("not found");
+
+    }
+
+}
+
+
